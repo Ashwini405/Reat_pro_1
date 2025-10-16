a4a11bb3e4d78155484cc8eb58dc2f0c532000d5
Kenguva Ashwini <panianu8@gmail.com>
2025-10-16 17:14:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,31 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My First React App </h1>
+      <p>This app shows basic HTML tags inside React.</p>
+
+     
+
+      
+
+      <img
+        src="https://reactjs.org/logo-og.png"
+        alt="React Logo"
+        width="200"
+      />
+      <h2>Advantages:</h2>
+
+      <ul>
+        <li> Easy to use</li>
+        <li>Fast rendering</li>
+        <li> Reusable components</li>
+      </ul>
+      <br></br>
+
+      
+    </div>
+  );
+}
+
+export default App;
